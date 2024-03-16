960faca8d4a2e18cdd60736e7c7987d80317f85b
Rafig <rafigdzabbarov0410@gmail.com>
2024-03-16 03:59:28 +0300

    init Task1

---
+++ Task1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() 
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>
+            {
+                { "1", "2" }
+            };
+            string a = "1";
+            Console.WriteLine("HashCode: " + a.GetHashCode());
+            InitializeComponent();
+        }
+    }
+}
